e215fbeae499b6354ada39d8972529a59f8d9bb0
vladikmbojole <71111635+vladikmbojole@users.noreply.github.com>
2020-10-25 22:46:39 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let add = document.getElementById('increment');
+let remove = document.getElementById('decrement');
+
+let int = document.getElementById('number');
+let integer = 0;
+
+add.addEventListener('click', function(){
+    integer += 1;
+    int.innerHTML = integer;
+})
+
+remove.addEventListener('click', function(){
+    if (integer>=1) {
+        integer -= 1;
+        int.innerHTML = integer;
+    }
+    
+})
+
+
+
+
+
